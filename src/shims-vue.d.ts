942211a568a4e2d504efff345f62cc69732b0dbb
beiCheNzero <2511782938@qq.com>
2022-11-01 11:11:51 +0800

    style: little bug-.vue file declare
    coderli change

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,8 +1,8 @@
 /* eslint-disable */
 declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
+  import { defineComponent } from 'vue'
+  const Component: ReturnType<typeof defineComponent>
+  export default Component
 }
 declare let $filters: any
 // declare let $store: any
